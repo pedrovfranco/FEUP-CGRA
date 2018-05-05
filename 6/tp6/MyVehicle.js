629b05065d46035c35e0be26644f679aa51e05c0
pedrofixe <yurbypedro@hotmai.com>
2018-05-05 01:46:20 +0100

    Minor changes

--- 6/tp6/MyVehicle.js
+++ 6/tp6/MyVehicle.js
@@ -14,25 +14,22 @@ class MyVehicle extends CGFobject
 		this.body = new MyPrismWithTop(scene, 4, 1);
 		this.tire = new MyTire(scene, 35, 1);
 
-		var scaleX = 1;
-		var scaleY = 1;
-
-		this.plane = new Plane(scene, 20, 0 - (scaleY-1)/2, 1 + (scaleY-1)/2, 0 - (scaleX-1)/2, 1 + (scaleX-1)/2, 0);
-		
+		this.length = 4.3;
+		this.tireDiameter = 1;
+		this.thickness = 2.5
+		this.height = 1.8;		
 	};
 
 	display()
 	{
 
-		var length = 4.3, tireDiameter = 1, thickness = 2.5, height = 1.8;
-
-		var tireScale = tireDiameter/2;
+		var tireScale = this.tireDiameter/2;
 		var tireThickness = tireScale*0.7;
-		var tireX = (thickness - 2*tireThickness)/2 - 0.01;
+		var tireX = (this.thickness - 2*tireThickness)/2 - 0.01;
 
 		// //back left tire
 		this.scene.pushMatrix();
-			this.scene.translate(tireX, tireScale, -length/2);
+			this.scene.translate(tireX, tireScale, -this.length/2);
 			this.scene.rotate(Math.PI/2, 0, 1, 0);
 			this.scene.scale(tireScale, tireScale, tireThickness);
 			this.tire.display();
@@ -40,7 +37,7 @@ class MyVehicle extends CGFobject
 
 		// //back right tire
 		this.scene.pushMatrix();
-			this.scene.translate(-tireThickness-tireX, tireScale, -length/2);
+			this.scene.translate(-tireThickness-tireX, tireScale, -this.length/2);
 			this.scene.rotate(Math.PI/2, 0, 1, 0);
 			this.scene.scale(tireScale, tireScale, tireThickness);
 			this.tire.display();
@@ -48,7 +45,7 @@ class MyVehicle extends CGFobject
 
 		//front left tire
 		this.scene.pushMatrix();
-			this.scene.translate(tireX, tireScale, length/2);
+			this.scene.translate(tireX, tireScale, this.length/2);
 			this.scene.rotate(Math.PI/2, 0, 1, 0);
 			this.scene.scale(tireScale, tireScale, tireThickness);
 			this.tire.display();
@@ -56,7 +53,7 @@ class MyVehicle extends CGFobject
 		
 		//front right tire
 		this.scene.pushMatrix();
-			this.scene.translate(-tireThickness-tireX, tireScale, length/2);
+			this.scene.translate(-tireThickness-tireX, tireScale, this.length/2);
 			this.scene.rotate(Math.PI/2, 0, 1, 0);
 			this.scene.scale(tireScale, tireScale, tireThickness);
 			this.tire.display();
@@ -65,7 +62,7 @@ class MyVehicle extends CGFobject
 		//body
 		this.scene.pushMatrix();
 			this.scene.translate(0, 0.6, 0);
-			this.scene.scale(thickness/Math.sqrt(2), 0.9, length);
+			this.scene.scale(this.thickness/Math.sqrt(2), 0.9, this.length);
 			this.scene.rotate(-Math.PI/2, 1, 0, 0);
 			this.scene.rotate(Math.PI/4, 0, 0, 1);
 			this.body.display();
@@ -74,7 +71,7 @@ class MyVehicle extends CGFobject
 		//top
 		this.scene.pushMatrix();
 			this.scene.translate(0, 0.6 + 0.9, 0);
-			this.scene.scale(thickness, 0.8, length/2/1.2);
+			this.scene.scale(this.thickness, 0.8, this.length/2/1.2);
 			this.scene.translate(-0.5, 0.5, 0);
 			this.scene.rotate(Math.PI/2, 0, 1, 0);
 			this.top.display();
