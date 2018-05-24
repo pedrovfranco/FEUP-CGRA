1fb2ffba16a5a6b760cf1e1e7c17484fd7874c05
Gwenevere <33674643+Gwenevere@users.noreply.github.com>
2018-05-24 19:37:46 +0100

    Added car to crane

--- 6/tp6/MyCrane.js
+++ 6/tp6/MyCrane.js
@@ -28,9 +28,6 @@
 
 	display(){
 
-	// this.scene.rotate(-this.angle, 1,0,0);
-//	this.scene.translate(0.55,7.5,-4);
-
 	this.scene.pushMatrix();
    	this.scene.scale(1/3,1/3,4);
       this.scene.translate(0, 1, -1);
@@ -114,7 +111,7 @@ class MyCrane extends CGFobject
 		this.smallerBase = new MyCylinderForTexture(scene,slices,stacks);
 		this.arm = new MyPrismWithTop(scene, 4, 1);
 		this.topCrane = new MyUpperCrane(scene,slices,stacks);
-      this.pendulum = new MyPendulum(scene, slices, stacks);
+      	this.pendulum = new MyPendulum(scene, slices, stacks);
 
 		this.baseAppearance = new CGFappearance(this.scene);
 		this.baseAppearance.loadTexture("../resources/images/black.png");
@@ -138,11 +135,13 @@ class MyCrane extends CGFobject
 
 		this.deltaTime = -1;
 
+		this.car = new MyVehicle(scene); 
+
 	}
 
 	display(){
 
-//	this.scene.rotate(this.angle, 0,1,0);
+	this.scene.rotate(this.angle, 0,1,0);
 
 	this.scene.pushMatrix();
 	this.baseAppearance.apply();
@@ -172,7 +171,6 @@ class MyCrane extends CGFobject
 	this.scene.popMatrix();
 
    this.scene.pushMatrix();
-   // this.scene.translate(0, -1.3, -0.05);
 	  this.scene.translate(this.pendulumPostition[0], this.pendulumPostition[1], this.pendulumPostition[2]);
 	  this.pendulum.display();
 	this.scene.popMatrix();
@@ -201,21 +199,27 @@ class MyCrane extends CGFobject
 	updatePosition()
 	{
 
-		if(this.angle <=  this.maxAngle)
-      {
+		if (this.angle <=  this.maxAngle)
+		{
 			this.angle += this.rotationSpeed;
 		}
+		else
+		{
+			if(this.topCraneAngle >=  this.topCraneMaxAngle)
+      		{
+				this.topCraneAngle -= this.deltaTime/2;
+			}
 
-      if(this.topCraneAngle >=  this.topCraneMaxAngle)
-      {
-			this.topCraneAngle -= this.deltaTime/2;
 		}
+      
+		  this.pendulumPostition[0] = this.topCranePosition[0];
+		  this.pendulumPostition[1] = this.topCranePosition[1] + 4*Math.sin(this.topCraneAngle);
+		  this.pendulumPostition[2] = this.topCranePosition[2] - 4*Math.cos(this.topCraneAngle);
+	}
 
-      this.pendulumPostition[0] = this.topCranePosition[0];
-      this.pendulumPostition[1] = this.topCranePosition[1] + 4*Math.sin(this.topCraneAngle);
-      this.pendulumPostition[2] = this.topCranePosition[2] - 4*Math.cos(this.topCraneAngle);
-
-      console.log(this.topCraneAngle);
+	setCar(car)
+	{
+		this.car = car
 	}
 
 };
